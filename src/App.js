e52cacb3122a3fff941395c073d00806d2696aec
adriantuftisdahl <ati_37@hotmail.com>
2019-04-28 21:13:08 +0200

    An exclamation mark!

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React man
+          Learn React man!
         </a>
       </header>
     </div>
